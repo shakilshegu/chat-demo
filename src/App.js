03d38ceb1a9bb73a3028d64b9c07bb43e2f386d8
shakilshagu <muhammadshakil1968@gmail.com>
2024-02-05 16:53:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { CometChat } from "@cometchat-pro/chat"
+import { CometChatUI } from "./cometchat-pro-react-ui-kit/CometChatWorkspace/src/components"
 
+const appID = process.env.REACT_APP_COMETCHAT_APP_ID
+const region = process.env.REACT_APP_COMETCHAT_REGION
+
+const appSetting = new CometChat.AppSettingsBuilder().subscribePresenceForAllUsers().setRegion(region).build();
+CometChat.init(appID,appSetting).then(
+  ()=>{
+    console.log("Initilization complete successfuly");
+  },
+  error => {
+    console.log("Initilization faild",error);
+  }
+)
+
+// create uer function
+
+const authKey = process.env.REACT_APP_COMETCHAT_AUTH_KEY
+
+const uid = "user2"
+// const name = "rizwan";
+
+// var user = new CometChat.User(uid)
+// user.setName(name);
+// CometChat.createUser(user, authKey).then(
+//   user => {
+//     console.log("user created",user);
+//   },error =>{
+//     console.log("error creating user",error);
+//   }
+// )
+
+//* login user functions
+
+CometChat.login(uid,authKey).then(
+  user => {
+    console.log("User logged in Successfully",{user});
+  },
+  error => {
+    console.log("login failed ",{error});
+  }
+)
+ 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '800px', height: '700px', margin: 'auto' }}>
+    <CometChatUI/>
     </div>
   );
 }
